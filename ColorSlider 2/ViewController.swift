01be9399e98d844eecc20a2dc94727139858f131
Eugenia <pyssiki@yandex.ru>
2022-03-18 23:01:10 +0500

    Initial Commit

---
+++ ColorSlider 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorSlider 2
+//
+//  Created by Евгения Аникина on 18.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
